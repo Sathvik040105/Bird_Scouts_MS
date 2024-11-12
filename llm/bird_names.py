8fd2555b5ecb11bb563627a90bdedf7909a9cd49
ELNKrishna <krishnal@iisc.ac.in>
2024-11-12 20:46:29 +0530

    Changed scientific names to common names

--- llm/bird_names.py
+++ llm/bird_names.py
@@ -1,5 +1,13 @@
 birds = [
-    "House_crow",
-    "Peafowl",
-    "House_sparrow"
+    'Asian_green_bee-eater',
+    'Brown-headed_barbet',
+    'Cattle_egret',
+    'Common_kingfisher',
+    'Common_myna',
+    'Common_rosefinch',
+    'Common_tailorbird',
+    'Coppersmith_barbet',
+    'Forest_wagtail',
+    'Gray_wagtail',
+    'Hoopoe',
 ]
